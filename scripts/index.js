07a6300e6c66823fde6908f7e6558e8f7fec5325
Miguel <angelfalconmv@gmail.com>
2025-01-23 23:09:48 -0300

    mejorando las clases segun reglas BEM

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
-// Creo las variables que necesito para seleccionar los elements__elementos del DOM que me interesan.
+// Creo las variables que necesito para seleccionar los elements__itemos del DOM que me interesan.
 const botonEditar = document.querySelector(".profile__info__header__button");
-const botonAdd = document.querySelector(".profile__add__button__edit");
+const botonAdd = document.querySelector(".profile__add-button-edit");
 const formPopUp = document.querySelector(".form");
 const botonCierre = document.querySelector(".form__button-close");
-const inputNombre = document.querySelector(".form__edit__input__text");
+const inputNombre = document.querySelector(".form__edit-input-name");
 const inputDescripcion = document.querySelector(
-  ".form__edit__input__description"
+  ".form__edit-input-description"
 );
-const Nombre = document.querySelector(".form__edit__input__text");
-const Descripcion = document.querySelector(".form__edit__input__description");
+
 const form = document.getElementById("form");
 const imgCorazon = document.querySelectorAll(
-  ".elements__element__content__corazon"
+  ".elements__item-like-button"
 );
-const botonForm = document.querySelector(".form__edit__input__button");
+const botonForm = document.querySelector(".form__edit-submit-button");
+const Nombre = document.querySelector(".profile__info-header-title");
+const Descripción = document.querySelector(".profile__info-details-text");
 
 // Añado un event listener al botón de editar para que cuando se haga click se ejecute la función abrirFormulario.
 botonEditar.addEventListener("click", abrirFormulario);
@@ -32,13 +33,14 @@ form.addEventListener("submit", guardarDatos);
 function guardarDatos(e) {
   e.preventDefault();
   Nombre.textContent = inputNombre.value;
-  Descripcion.textContent = inputDescripcion.value;
+  Descripción.textContent = inputDescripcion.value;
+  formPopUp.classList.remove("form__show");
 }
 
 inputNombre.addEventListener("input", botonActivado);
 inputDescripcion.addEventListener("input", botonActivado);
 function botonActivado() {
-  botonForm.classList.add("form__edit__input__button--active");
+  botonForm.classList.add("form__edit-submit-button--active");
 }
 
 // Cambiar color del corazón
