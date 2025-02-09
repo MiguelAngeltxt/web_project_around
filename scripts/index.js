cdd5722d1020eda4af092f48f925fcc6bf84c2b1
Miguel Falcon <angelfalconmv@gmail.com>
2025-02-09 19:05:04 -0300

    dando funcionalidad al boton eliminar

--- scripts/index.js
+++ scripts/index.js
@@ -11,13 +11,17 @@ const botonCierre = document.querySelector(".form__button-close");
 const inputFirst = document.querySelector(".form__edit-input-first");
 //selecciona el segundo input de texto del formulario
 const inputSecond = document.querySelector(".form__edit-input-second");
-
+//selecciono el formulario
 const form = document.getElementById("form");
+//selecciono el boton para enviar el formulario
 const botonForm = document.querySelector(".form__edit-submit-button");
+//nombre del usuario
 const Nombre = document.querySelector(".profile__info-header-title");
+//descripcion del usuario
 const Descripción = document.querySelector(".profile__info-details-text");
 
-let isEditMode = false; // Variable de estado para saber si estamos en modo de edición
+// Variable de estado para saber si estamos en modo de edición
+let isEditMode = false;
 
 // Función para abrir el formulario y modificar los textos y placeholders según el tipo de formulario
 function abrirFormulario(
@@ -47,11 +51,12 @@ botonAdd.addEventListener("click", () =>
   abrirFormulario("Nuevo lugar", "Título", "Enlace a la imagen", false)
 );
 
-//implemeta aca el boton de cierre del formulario
+//boton de cierre del formulario
 botonCierre.addEventListener("click", () =>
   formPopUp.classList.toggle("form__show")
 );
 
+//hago que se active el boton submit
 inputFirst.addEventListener("input", botonActivado);
 inputSecond.addEventListener("input", botonActivado);
 function botonActivado() {
@@ -89,14 +94,19 @@ const initialCards = [
 const cardContainer = document.querySelector(".elements");
 //agrega aqui la informacion de las cards.
 function addCard(name, link) {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate
+  const cardTemplate = document.querySelector("#card-template").content; //selecciono el template
+  const cardElement = cardTemplate //clono el template
     .querySelector(".elements__item")
     .cloneNode(true);
-  const likeButton = cardElement.querySelector(".elements__item-like-button");
-
+  //establezco los datos que iran en cada input
   cardElement.querySelector(".elements__item-img").src = link;
   cardElement.querySelector(".elements__item-title").textContent = name;
+  //incorporo cada card
+  cardContainer.append(cardElement);
+
+  //variable del boton like
+  const likeButton = cardElement.querySelector(".elements__item-like-button");
+  // le doy funcionalidad al boton like
   likeButton.addEventListener("click", () => {
     if (likeButton.src.includes("Group.png")) {
       likeButton.src = "./images/Union.png";
@@ -104,7 +114,7 @@ function addCard(name, link) {
       likeButton.src = "./images/Group.png";
     }
   });
-
+  //establezco la apertura de la imagen mediante su click
   cardElement
     .querySelector(".elements__item-img")
     .addEventListener("click", () => {
@@ -114,15 +124,19 @@ function addCard(name, link) {
       document.querySelector(".popup__image-img").src = link;
       document.querySelector(".popup__image-text").textContent = name;
     });
-  cardContainer.append(cardElement);
-  // const botonDelete = document.querySelector(".elements__item-action-button");
-  // botonDelete.addEventListener("click", () => {
-  //   cardElement.remove();
-  // });
+  //selecciono el boton eliminar
+  const deleteButton = cardElement.querySelector(
+    ".elements__item-action-button"
+  );
+  //funcionalidad del boton eliminar
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
 }
 
+//selecciono el boton para cerrar el popup de imagen
 const cerrarImagen = document.querySelector(".popup__button-close");
-
+//doy funcionalidad al boton para que se cierre la imagen
 cerrarImagen.addEventListener("click", () => {
   document.querySelector(".popup__image").classList.remove("popup__image-show");
 });
@@ -131,7 +145,7 @@ cerrarImagen.addEventListener("click", () => {
 form.addEventListener("submit", (e) => {
   // Se previene la acción predeterminada del formulario (recargar la página)
   e.preventDefault();
-
+  //distingo las dos formas de abrir el formulario
   if (isEditMode) {
     Nombre.textContent = inputFirst.value;
     Descripción.textContent = inputSecond.value;
